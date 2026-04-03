68146edaeffa6e64e08d0ff6dbd6dc6c24e8be0e
Ghassan Farajallah <YOUR_GITHUB_EMAIL>
2026-04-03 19:19:11 +0400

    feat: replace solutions blades backgrounds with the user's selected images

--- src/pages/Solutions.jsx
+++ src/pages/Solutions.jsx
@@ -22,7 +22,7 @@ const Solutions = () => {
       icon: <Globe size={48} />,
       link: "/solutions/etmam",
       color: "rgba(59, 130, 246, 0.5)",
-      bgImage: "/media/consultancy_bg.jpg"
+      bgImage: "/media/final_blade_consultancy.jpg"
     },
     {
       id: "smart-blocks",
@@ -35,7 +35,7 @@ const Solutions = () => {
       icon: <Cpu size={48} />,
       link: "/solutions/smartblocks",
       color: "rgba(201, 169, 98, 0.5)",
-      bgImage: "/media/smartblocks_bg.jpg"
+      bgImage: "/media/final_blade_smartblocks.jpg"
     },
     {
       id: "smart-agent",
@@ -48,7 +48,7 @@ const Solutions = () => {
       icon: <Bot size={48} />,
       link: "/solutions/smartagent",
       color: "rgba(34, 211, 238, 0.5)",
-      bgImage: "/media/smartagent_bg.jpg"
+      bgImage: "/media/final_blade_smartagent.png"
     }
   ];
 
